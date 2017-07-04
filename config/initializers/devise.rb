d98090ed80e51e0bf349ecab0113e95c089c4740
Flippylolz <assasin463@gmail.com>
2017-07-04 12:43:49 +0300

    Set up Puma, Nginx & Capistrano + devise keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,1 +1,15 @@
-Devise.secret_key = "d5c9f5395c7033952556ed89c13d0b7a04eb652032625c71e4e26b18cd5aa59caa9a410cbe132e12c47426f7d5ac0ebc8a05"
+Devise.setup do |config|
+  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  require 'devise/orm/active_record'
+  config.case_insensitive_keys = [:email]
+  config.strip_whitespace_keys = [:email]
+  config.skip_session_storage = [:http_auth]
+  config.stretches = Rails.env.test? ? 1 : 11
+  config.reconfirmable = true
+  config.expire_all_remember_me_on_sign_out = true
+  config.password_length = 6..128
+  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  config.reset_password_within = 6.hours
+  config.sign_out_via = :delete
+  config.secret_key = 'badf439ada9fe9c8a352b176158beba97153a4a53cc76a266dbda3625caa28505c8063c729dc52a38119640625fcb54ffb917cc8fc4ab9a9b13818328ffa7640'
+end
